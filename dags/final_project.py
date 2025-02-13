0a7ac45cef0f42fbcbb98287070fee8e76fff94f
Mrluvnagpal <168093134+Mrluvnagpal@users.noreply.github.com>
2025-02-13 02:54:02 +0000

    Update final_project.py

--- dags/final_project.py
+++ dags/final_project.py
@@ -6,7 +6,7 @@ from operators.stage_redshift import StageToRedshiftOperator
 from operators.load_fact import LoadFactOperator
 from operators.load_dimension import LoadDimensionOperator
 from operators.data_quality import DataQualityOperator
-from udacity.common.final_project_sql_statements import SqlQueries
+from helpers.sql_queries import SqlQueries
 
 default_args = {
     'owner': 'udacity',
